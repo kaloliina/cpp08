bcb25f7750f54e4a2b70dc68896d65bf526a7fe2
Karoliina Hiidenheimo <khiidenh@c1r5p7.hive.fi>
2026-02-20 16:28:59 +0200

    attempting to draft the final exercise

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -1,7 +1,31 @@
+#include <iostream>
+#include <stack>
+/*Stack container follows LIFO (last in first out) order of insertion and deletion.
+It means that most recently inserted element is removed first and the first inserted element will
+be removed last. This is done by inserting and deleting elements at only one end of the stack
+which is generally called the top of the stack.
+stack<T> stdL (T being data type of elements in the stack)
+Push for inserting elements, pop for deleting, empty, size*/
 
 
-class MutantStack
+//can we not inherit from std::stack??
+
+/*this stack can contain any data types so thats why templates...*/
+//if stack doesnt have T there, main doesnt recognize the stack's functions
+template <typename T>
+class MutantStack : public std::stack<T>
 {
-	
+	public:
+//	iterator it;
+	iterator begin()
+	{
+		stack<int> tempt(st);
+		while (!temp.empty())
+		{
+			std::cout << temp.top() << " ":
+			temp.pop();
+		}
+		return  temp;
+	}
 
 };
